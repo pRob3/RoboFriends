4a49b781e2be239a3a56a40dd0c1aef6055c41eb
pRob3 <hundraprocent@me.com>
2020-07-28 11:17:12 +0200

    Added searchbox component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import CardList from './CardList';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
 import { robots } from './robots';
@@ -13,9 +13,7 @@ import { robots } from './robots';
 //   document.getElementById('root')
 // );
 
-ReactDOM.render(
-<CardList robots={robots} />
-    , document.getElementById('root'));
+ReactDOM.render(<App /> , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
