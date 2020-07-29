e49934f4efe873d0fb9d53353ff118fd8a218583
pRob3 <hundraprocent@me.com>
2020-07-29 13:46:15 +0200

    Added error boundry component to catch errors

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
+import ErrorBoundry from '../components/ErrorBoundry'
 import './App.css';
 
 
@@ -15,21 +16,21 @@ class App extends Component {
     }
 
 
-    componentDidMount(){
+    componentDidMount() {
         fetch('https://jsonplaceholder.typicode.com/users')
-        .then(response => response.json())
-        .then(users => {this.setState({ robots: users })});
+            .then(response => response.json())
+            .then(users => { this.setState({ robots: users }) });
     }
 
 
     onSearchChange = (event) => {
-        this.setState({searchfield: event.target.value})
+        this.setState({ searchfield: event.target.value })
     }
 
 
     render() {
         const { robots, searchfield } = this.state;
-        const filteredRobots = robots.filter(robot =>{
+        const filteredRobots = robots.filter(robot => {
             return robot.name.toLowerCase().includes(searchfield.toLowerCase());
         })
         return !robots.length ?
@@ -39,7 +40,9 @@ class App extends Component {
                     <h1 className='f1'>RoboFriends</h1>
                     <SearchBox searchChange={this.onSearchChange} />
                     <Scroll>
-                        <CardList robots={filteredRobots} />
+                        <ErrorBoundry>
+                            <CardList robots={filteredRobots} />
+                        </ErrorBoundry>
                     </Scroll>
                 </div>
             );
