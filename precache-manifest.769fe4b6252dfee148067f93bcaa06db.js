bd65224d9d5dc91b0d031b8137d7f4df356c7907
pRob3 <hundraprocent@me.com>
2020-08-03 18:45:01 +0200

    Revert "Updates"
    This reverts commit a452cb61de17badb821bca0b96d12f2d2fdda577.

---
+++ precache-manifest.769fe4b6252dfee148067f93bcaa06db.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9185d5a9ff410078ee0a8011efbaeb29",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "3e53305b342f1a535056",
+    "url": "/robofriends/static/css/main.4ec93c41.chunk.css"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/robofriends/static/js/2.cab51068.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.cab51068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e53305b342f1a535056",
+    "url": "/robofriends/static/js/main.ee7ef8b8.chunk.js"
+  },
+  {
+    "revision": "e7d2cbb368e0813da61d",
+    "url": "/robofriends/static/js/runtime-main.74eee7b5.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "265e83321e161f3d7036974ae100fa05",
+    "url": "/robofriends/static/media/game_over.265e8332.woff"
+  }
+]);
